0f03911e74f7d54a3e8d097a936c96dcb719f810
peizhouyu <peizhouyu@live.com>
2018-02-03 12:07:09 +0800

    first commit

---
+++ web-server/simple-server/src/main/java/cn/mrpei/simple/HttpServer.java
@@ -0,0 +1,54 @@
+package cn.mrpei.simple;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+
+    public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+
+    private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+
+    private boolean shutdown = false;
+
+    public static void main(String[] args) {
+        HttpServer server = new HttpServer();
+        server.await();
+    }
+
+    public void await(){
+        ServerSocket serverSocket = null;
+        int port = 8080;
+        try {
+            serverSocket = new ServerSocket(port,1, InetAddress.getByName("127.0.0.1"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        while (!shutdown){
+            Socket socket = null;
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try {
+                socket = serverSocket.accept();
+                inputStream = socket.getInputStream();
+                outputStream = socket.getOutputStream();
+
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+
+}
